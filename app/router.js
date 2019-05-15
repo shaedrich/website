9b8c3d091da55f1ca20b09001e3a37f276c4008b
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-05-15 08:05:32 -0400

    properly highlight... still weird though

--- app/router.js
+++ app/router.js
@@ -3,7 +3,14 @@ import config from './config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+
+  init() {
+    this._super(...arguments);
+    this.on('routeDidChange', () => {
+      window.Prism && window.Prism.highlightAll();
+    });
+  },
 });
 
 Router.map(function() {
